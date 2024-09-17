620b99ceb82d257ad438eae9e39a22f49c16a50c
AndrewLKrause <nightmartyr@gmail.com>
2024-09-17 22:23:42 +0100

    Updated files

--- runAndPlot.m
+++ runAndPlot.m
@@ -1,25 +1,38 @@
 %clear
-rng(1);
-%subcritical = 1; %Use this only if f is a homogeneous function with a subcritical instability and qc is 1.
-%f = @(U)1.5*U.^3-1*U.^5;
-%F = @(U)1.5/4*U.^4-(1/6)*U.^6;
-% f = @(U)2*U.^2-U.^3;
-% F = @(U)(2/3)*U.^3-(1/4)*U.^4;
-% eps = 1e-2;
+rng(3);
+subcritical = 0; %Use this only if f is a homogeneous function with a subcritical instability and qc is 1.
+f = @(U)-U.^3;
+%f = @(U)2*U.^3-U.^5;
+F = @(U)(1/2)*U.^4-(1/6)*U.^6;
+ %f = @(U)2*U.^2-U.^3;
+ %F = @(U)(2/3)*U.^3-(1/4)*U.^4;
+ eps = 2e-4;
 
-N = 10000; T = [0,linspace(100,5000,1e2)];
+N = 50000; T = [0,linspace(10000,20000,3)];
 dx = 1/(N-1); % Spacing between grid points
 params.dx = dx; params.N = N; params.eps = eps; params.T = T;
 params.tol = 1e-6; 
 x = linspace(0,1,N)'; 
-%rf = @(x)-cos(2*pi*x);
-%rf = @(x)-0.4*cos(2*pi*x);
+%rf = @(x)-cos(2*pi*x)*0-0.7;
+%rf = @(x)-cos(2*pi*x)*0+1;
+
+%rf = @(x)-cos(20*pi*x.^2);
+rf = @(x)-cos(2*pi*x);
 r = rf(x);
 
 Q = @(x)x*0+1;
 qc = Q(x);
 
-U = RunSimulation(r, qc, f, params,1e-2*randn(params.N,1));
+%U = RunSimulation(r, qc, f, params,1e-2*randn(params.N,1));
+
+%Either load the initial condition from a file, or use a small
+%normally-distributed random one.
+%load('init3.mat');load('init_eps_2e-3.mat');
+Us = 1e-2*randn(params.N,1);
+%load('init_eps_2e-4.mat');
+
+
+U = RunSimulation(r, qc, f, params,Us);
 
 
 if(subcritical && ~exist('rc'))
@@ -30,3 +43,6 @@ end
 
 
 plotSols;
+
+
+
